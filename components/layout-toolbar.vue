0aaf1cbbb88c5b78590cd4e79aa3c438718ee265
sergio.gordo <sergio@meta-site.com>
2019-07-31 19:43:45 +0200

    new_web_init

---
+++ components/layout-toolbar.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "layout-toolbar"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/program2.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-layout>
+    <v-container v-bind="{ [`grid-list-${size}`]: true }" fluid>
+      <v-toolbar color="transparent" :flat="true" :align-with-title="true" tabs>
+        <template v-slot:extension>
+          <v-tabs v-model="tab" color="transparent" grow>
+            <v-tabs-slider color="#d13b54"></v-tabs-slider>
+
+            <v-tab v-for="item in posts" :key="item.fields.title">
+              {{ item.fields.title }}
+            </v-tab>
+          </v-tabs>
+        </template>
+      </v-toolbar>
+      <v-tabs-items v-model="tab">
+        <v-tab-item v-for="item in posts" :key="item.fields.title">
+          <v-card flat color="transparent">
+            <v-card-text v-html="documentToHtmlString(item.fields.programa)">
+            </v-card-text>
+          </v-card>
+        </v-tab-item>
+      </v-tabs-items>
+    </v-container>
+  </v-layout>
+</template>
+
+<script>
+import { documentToHtmlString } from "@contentful/rich-text-html-renderer";
+
+export default {
+  name: "program",
+  data: () => ({
+    size: "sm",
+    tab: null,
+      documentToHtmlString: documentToHtmlString,
+    items: [
+      { text: "Extra small (2px)", value: "xs" },
+      { text: "Small (4px)", value: "sm" },
+      { text: "Medium (8px)", value: "md" },
+      { text: "Large (16px)", value: "lg" },
+      { text: "Extra large (24px)", value: "xl" }
+    ]
+  }),
+  computed: {
+    posts() {
+      return this.$store.state.programa;
+    }
+  },
+  created() {
+    this.$store.dispatch("getEntriesAction", "programa");
+  }
+};
+</script>
+
+<style scoped></style>
